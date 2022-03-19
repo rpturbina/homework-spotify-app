b8bb32b6b8ae299605cbe1a7317de400ebb4d033
rpturbina <rizki.pratama.turbina@gmail.com>
2022-03-19 11:58:56 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+const ENDPOINT =
+  "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json";
+
+const SPOTIFY_API_KEY = process.env.REACT_APP_SPOTIFY_KEY;
+
+const getResponse = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getResponse();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
