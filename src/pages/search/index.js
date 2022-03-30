7f65be4adc7f333f0b27c230396e2fba0120b6c5
rpturbina <rizki.pratama.turbina@gmail.com>
2022-03-30 21:04:56 +0700

    convert from class comp to func component

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,82 +1,74 @@
-import { Component } from "react";
+import { useState } from "react";
 import TableOfTracks from "../../components/TableOfTracks";
 
-class SearchSong extends Component {
-  state = { access_token: "", searchInput: "", searchResult: [] };
+const SearchSong = () => {
+  const [accessToken, setAccessToken] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
 
-  getSongs = async (event) => {
-    const currentLocation = String(window.location).split("#")[1];
-    const params = new URLSearchParams(currentLocation);
-    let ACCESS_TOKEN = params.get("access_token");
-    this.setState({ access_token: ACCESS_TOKEN });
-    console.log(ACCESS_TOKEN);
-    console.log("Access token telah didapatkan.");
-
-    const fetchOptions = {
-      method: "GET",
-      headers: new Headers({
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
-      }),
-    };
-
-    const LIMIT = 10;
-    const queryTerm = this.state.searchInput;
-    let api_endpoint = `https://api.spotify.com/v1/search?type=track&q=${queryTerm}&limit=${LIMIT}`;
-
-    const result = await fetch(api_endpoint, fetchOptions).then((response) => response.json());
-    this.setState({ searchResult: result.tracks.items });
-    // console.log(result.tracks.items);
+  const generateRandomString = (length = 16) => {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let str = "";
+    for (let i = 0; i < length; i++) {
+      str += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return str;
   };
 
-  handleInput = (event) => this.setState({ searchInput: event.target.value });
+  const getAccessToken = async () => {
+    const currentLocation = String(window.location).split("#")[1];
+    const params = new URLSearchParams(currentLocation);
+    const ACCESS_TOKEN = params.get("access_token");
+    setAccessToken(ACCESS_TOKEN);
+  };
 
-  render() {
-    const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-    const redirect_uri = "http://localhost:3000/";
+  const getSongs = async () => {
+    try {
+      getAccessToken();
+      const fetchOptions = {
+        method: "GET",
+        headers: new Headers({
+          Authorization: `Bearer ${accessToken}`,
+        }),
+      };
 
-    const generateRandomString = (length = 16) => {
-      // Declare all characters
-      let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      const LIMIT = 10;
+      const queryTerm = searchInput;
+      const api_endpoint = `https://api.spotify.com/v1/search?type=track&q=${queryTerm}&limit=${LIMIT}`;
 
-      // Pick characters randomly
-      let str = "";
-      for (let i = 0; i < length; i++) {
-        str += chars.charAt(Math.floor(Math.random() * chars.length));
-      }
+      const result = await fetch(api_endpoint, fetchOptions).then((response) => response.json());
+      setSearchResult(result.tracks.items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-      return str;
-    };
+  const handleInput = (event) => setSearchInput(event.target.value);
 
-    const state = generateRandomString(16);
+  const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const redirect_uri = "http://localhost:3000/";
+  const state = generateRandomString(16);
+  const scope = "playlist-modify-private";
 
-    const scope = "playlist-modify-private";
+  let url = "https://accounts.spotify.com/authorize";
+  url += "?response_type=token";
+  url += `&client_id=${encodeURIComponent(client_id)}`;
+  url += `&scope=${encodeURIComponent(scope)}`;
+  url += `&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  url += `&state=${encodeURIComponent(state)}`;
 
-    let url = "https://accounts.spotify.com/authorize";
-    url += "?response_type=token";
-    url += `&client_id=${encodeURIComponent(client_id)}`;
-    url += `&scope=${encodeURIComponent(scope)}`;
-    url += `&redirect_uri=${encodeURIComponent(redirect_uri)}`;
-    url += `&state=${encodeURIComponent(state)}`;
-
-    return (
-      <div className="search-bar">
-        <input
-          type="text"
-          onChange={this.handleInput}
-          value={this.state.searchInput}
-          className="input"
-          required
-        />
-        <button type="submit" onClick={this.getSongs} className="btn btn--select">
-          Search
-        </button>
-        <a href={url} className="btn">
-          Refresh Access
-        </a>
-        <TableOfTracks tracks={this.state.searchResult} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar">
+      <input type="text" onChange={handleInput} value={searchInput} className="input" required />
+      <button type="submit" onClick={getSongs} className="btn btn--select">
+        Search
+      </button>
+      <a href={url} className="btn">
+        {accessToken === "" ? "Login" : "Logout"}
+      </a>
+      <TableOfTracks tracks={searchResult} />
+    </div>
+  );
+};
 
 export default SearchSong;
