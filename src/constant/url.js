ea5e556a0f99eda89fb2422f5c9938200d7a759c
rpturbina <rizki.pratama.turbina@gmail.com>
2022-04-22 05:28:27 +0700

    refactor code and delete unused files

---
+++ src/constant/url.js
@@ -0,0 +1,9 @@
+/**
+ * Base url for Spotify Web API
+ */
+export const BASE_SPOTIFY_API_URL = "https://api.spotify.com/v1";
+
+/**
+ * Base url for spotify authorization
+ */
+export const BASE_SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
